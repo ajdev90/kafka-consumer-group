dbde7fab7a5863e76d91b901c12417df96c483b1
AJoshi <ajdev90@gmail.com>
2025-03-30 17:54:42 +0530

    Create consumer-group2.js

---
+++ consumer-group2.js
@@ -0,0 +1,26 @@
+const {Kafka} = require('@confluentinc/kafka-javascript').KafkaJS;
+console.log('this is consumer 2');
+const consumer = new Kafka().consumer({
+    'bootstrap.servers': 'clm-pun-wuyfmk:9092',
+    'group.id': 'new-consumer-2'
+});
+
+const run = async () => {
+    await consumer.connect();
+    await consumer.subscribe({ topics: ["new_topic"] });
+    consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                topic,
+                partition,
+                headers: message.headers,
+                offset: message.offset,
+                key: message.key?.toString(),
+                value: message.value.toString(),
+            });
+        }
+    });
+
+}
+
+run().catch(e => console.error(`Error: ${e.message}`, e))
