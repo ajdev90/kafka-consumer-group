ac003fd7251b662fdcc0776cc8ec76770766c9bf
AJoshi <ajdev90@gmail.com>
2025-03-30 18:21:48 +0530

    Create consumer-group-from-beginning.js

---
+++ consumer-group-from-beginning.js
@@ -0,0 +1,27 @@
+const {Kafka} = require('@confluentinc/kafka-javascript').KafkaJS;
+
+const consumer = new Kafka().consumer({
+    'bootstrap.servers': 'my-broker:9092',
+    'group.id': 'new-consumer-3',
+    'kafkaJS': { fromBeginning: true }
+});
+
+const run = async () => {
+    await consumer.connect();
+    await consumer.subscribe({ topics: ["new_topic"] });
+    consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                topic,
+                partition,
+                headers: message.headers,
+                offset: message.offset,
+                key: message.key?.toString(),
+                value: message.value.toString(),
+            });
+        }
+    });
+
+}
+
+run().catch(e => console.error(`Error: ${e.message}`, e))
